28ff78647329054822e97ea8798d3bc135da58fe
Neeraj Dalal <admin@nrjdalal.com>
2022-01-16 14:04:50 +0530

    fast pace

--- components/BiofitFAQ.jsx
+++ components/BiofitFAQ.jsx
@@ -1,3 +1,88 @@
+// /* This example requires Tailwind CSS v2.0+ */
+// import { Disclosure } from '@headlessui/react'
+// import { ChevronDownIcon } from '@heroicons/react/outline'
+
+// const faqs = [
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   {
+//     question: "What's the best thing about Switzerland?",
+//     answer:
+//       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+//   },
+//   // More questions...
+// ]
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
+// export default function BiofitFAQ() {
+//   return (
+//     <div className="bg-purple-800">
+//       <div className="px-4 py-12 mx-auto max-w-7xl sm:py-16 sm:px-6 lg:px-8">
+//         <div className="max-w-3xl mx-auto divide-y-2 divide-none">
+//           <h2 className="pb-8 text-3xl font-extrabold text-center text-white sm:text-4xl">
+//             Frequently asked questions
+//           </h2>
+//           <dl className="mt-6 space-y-6 divide-y divide-black divide-opacity-20">
+//             {faqs.map((faq) => (
+//               <Disclosure as="div" key={faq.question} className="pt-6">
+//                 {({ open }) => (
+//                   <>
+//                     <dt className="text-lg">
+//                       <Disclosure.Button className="flex items-start justify-between w-full text-left text-white">
+//                         <span className="font-medium text-white">
+//                           {faq.question}
+//                         </span>
+//                         <span className="flex items-center ml-6 h-7">
+//                           <ChevronDownIcon
+//                             className={classNames(
+//                               open ? '-rotate-180' : 'rotate-0',
+//                               'h-6 w-6 transform'
+//                             )}
+//                             aria-hidden="true"
+//                           />
+//                         </span>
+//                       </Disclosure.Button>
+//                     </dt>
+//                     <Disclosure.Panel as="dd" className="pr-12 mt-2">
+//                       <p className="text-base text-white">{faq.answer}</p>
+//                     </Disclosure.Panel>
+//                   </>
+//                 )}
+//               </Disclosure>
+//             ))}
+//           </dl>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
 /* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
@@ -33,29 +118,28 @@ const faqs = [
     answer:
       "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
   },
-  // More questions...
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function BiofitFAQ() {
+export default function Example() {
   return (
-    <div className="bg-purple-800">
+    <div className="bg-gray-100">
       <div className="px-4 py-12 mx-auto max-w-7xl sm:py-16 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto divide-y-2 divide-none">
-          <h2 className="pb-8 text-3xl font-extrabold text-center text-white sm:text-4xl">
+        <div className="mx-auto divide-y-2 divide-gray-200 max-w-7xl">
+          <h2 className="pb-8 text-3xl font-extrabold text-center text-gray-900 sm:text-4xl">
             Frequently asked questions
           </h2>
-          <dl className="mt-6 space-y-6 divide-y divide-black divide-opacity-20">
+          <dl className="mt-6 space-y-6 divide-y divide-gray-200">
             {faqs.map((faq) => (
               <Disclosure as="div" key={faq.question} className="pt-6">
                 {({ open }) => (
                   <>
                     <dt className="text-lg">
-                      <Disclosure.Button className="flex items-start justify-between w-full text-left text-white">
-                        <span className="font-medium text-white">
+                      <Disclosure.Button className="flex items-start justify-between w-full text-left text-gray-400">
+                        <span className="font-medium text-gray-900">
                           {faq.question}
                         </span>
                         <span className="flex items-center ml-6 h-7">
@@ -70,7 +154,7 @@ export default function BiofitFAQ() {
                       </Disclosure.Button>
                     </dt>
                     <Disclosure.Panel as="dd" className="pr-12 mt-2">
-                      <p className="text-base text-white">{faq.answer}</p>
+                      <p className="text-base text-gray-500">{faq.answer}</p>
                     </Disclosure.Panel>
                   </>
                 )}
