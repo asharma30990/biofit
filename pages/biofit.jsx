4323c1c5bdb1b5b5f6836204c186b3de29674fa9
Neeraj Dalal <admin@nrjdalal.com>
2022-01-17 15:30:54 +0530

    ig

--- pages/biofit.jsx
+++ pages/biofit.jsx
@@ -247,18 +247,17 @@ export default function Index() {
                 <dt>
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-800 text-white">
                     <svg
-                      class="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M5 11l7-7 7 7M5 19l7-7 7 7"
                       />
                     </svg>
                   </div>
@@ -281,18 +280,17 @@ export default function Index() {
                 <dt>
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-800 text-white">
                     <svg
-                      class="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
                       />
                     </svg>
                   </div>
@@ -314,18 +312,17 @@ export default function Index() {
                 <dt>
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-800 text-white">
                     <svg
-                      class="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                       />
                     </svg>
                   </div>
@@ -347,18 +344,17 @@ export default function Index() {
                 <dt>
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-purple-800 text-white">
                     <svg
-                      class="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
                       />
                     </svg>
                   </div>
