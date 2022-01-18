d1efd1e1a157ea7ab0d91c9d845f330b20f20314
Neeraj Dalal <admin@nrjdalal.com>
2022-01-18 21:44:25 +0530

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="select-none">
+      <div className="select-none font-montserrat">
         <Component {...pageProps} />
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
