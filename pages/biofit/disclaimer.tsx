f91c1b2a7a0753f68244867137bdfade598a34c6
Neeraj Dalal <admin@nrjdalal.com>
2022-01-19 14:11:33 +0530

    footer

--- pages/biofit/disclaimer.tsx
+++ pages/biofit/disclaimer.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
 const index = () => {
   return (
     <>
       <div className="mx-auto prose px-[5%] max-w-7xl pt-12 leading-5 text-xl">
-        <a className="" href="/biofit">
-          Back
-        </a>
+        <Link href="/biofit">
+          <a>Back</a>
+        </Link>
       </div>
 
       <div className="mx-auto prose px-[5%] max-w-7xl py-12 leading-5">
