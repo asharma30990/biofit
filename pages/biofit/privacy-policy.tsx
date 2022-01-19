ab14e6aae2ae0b7d22948c1f83cc3217c259dda5
Neeraj Dalal <admin@nrjdalal.com>
2022-01-19 13:52:40 +0530

    footer

--- pages/biofit/privacy-policy.tsx
+++ pages/biofit/privacy-policy.tsx
@@ -48,32 +48,126 @@ const index = () => {
 
         <h2>How we use your information</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms.
+          We use the information we collect in various ways, including to:
+          <br />
+          <br />
+          Provide, operate, and maintain our webste
+          <br />
+          <br />
+          Improve, personalize, and expand our webste
+          <br />
+          <br />
+          Understand and analyze how you use our webste
+          <br />
+          <br />
+          Develop new products, services, features, and functionality
+          <br />
+          <br />
+          Communicate with you, either directly or through one of our partners,
+          including for customer service, to provide you with updates and other
+          information relating to the webste, and for marketing and promotional
+          purposes
+          <br />
+          <br />
+          Send you emails
+          <br />
+          <br />
+          Find and prevent fraud
+          <br />
+          <br />
+          Log Files
+          <br /> <br />
+          Biofit Supplement follows a standard procedure of using log files.
+          These files log visitors when they visit websites. All hosting
+          companies do this and a part of hosting services' analytics. The
+          information collected by log files include internet protocol (IP)
+          addresses, browser type, Internet Service Provider (ISP), date and
+          time stamp, referring/exit pages, and possibly the number of clicks.
+          These are not linked to any information that is personally
+          identifiable. The purpose of the information is for analyzing trends,
+          administering the site, tracking users' movement on the website, and
+          gathering demographic information.
         </p>
 
         <h2>Advertising Partners Privacy Policies</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms.
+          You may consult this list to find the Privacy Policy for each of the
+          advertising partners of Biofit Supplement.
+          <br /> <br />
+          Third-party ad servers or ad networks uses technologies like cookies,
+          JavaScript, or Web Beacons that are used in their respective
+          advertisements and links that appear on Biofit Supplement, which are
+          sent directly to users' browser. They automatically receive your IP
+          address when this occurs. These technologies are used to measure the
+          effectiveness of their advertising campaigns and/or to personalize the
+          advertising content that you see on websites that you visit.
+          <br /> <br />
+          Note that Biofit Supplement has no access to or control over these
+          cookies that are used by third-party advertisers.
         </p>
 
         <h2>Third Party Privacy Policies</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms.
+          Biofit Supplement's Privacy Policy does not apply to other advertisers
+          or websites. Thus, we are advising you to consult the respective
+          Privacy Policies of these third-party ad servers for more detailed
+          information. It may include their practices and instructions about how
+          to opt-out of certain options.
+          <br /> <br />
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers'
+          respective websites.
         </p>
 
         <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms.
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+          <br /> <br />
+          Request that a business that collects a consumer's personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers.
+          <br /> <br />
+          Request that a business delete any personal data about the consumer
+          that a business has collected.
+          <br /> <br />
+          Request that a business that sells a consumer's personal data, not
+          sell the consumer's personal data.
+          <br /> <br />
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
         </p>
 
         <h2>GDPR Data Protection Rights</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its terms.
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+          <br /> <br />
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+          <br /> <br />
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+          <br /> <br />
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+          <br /> <br />
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions.
+          <br /> <br />
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+          <br /> <br />
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions.
+          <br /> <br />
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
         </p>
 
         <h2>Children's Information</h2>
