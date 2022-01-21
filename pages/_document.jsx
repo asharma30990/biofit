5a64885cc0071a9b4bcbd100f034a17fb9724281
Neeraj Dalal <admin@nrjdalal.com>
2022-01-21 16:22:50 +0530

    timer added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
+          <title>Biofit Official USA</title>
+          <description>
+            BioFit is an all-natural supplement that is known to enhance and
+            balance good bacteria in the gut area. To lose weight, you need to
+            have a balanced hormones and body processes. Many times, people
+            struggle with weight loss because their gut health has issues.
+          </description>
         </Head>
         <body>
           <Main />
