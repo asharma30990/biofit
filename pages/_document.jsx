fc7ab468cad6bc702f50fb26f3a5dcf072c16db9
nrjdalal <nrjdalal@nrjdalal.local>
2022-01-24 16:04:15 +0530

    gsv

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,25 +11,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
 
-          <title>Biofit Official USA</title>
-
-          <meta
-            name="description"
-            content="BioFit is an all-natural supplement that is known to enhance and
-            balance good bacteria in the gut area. To lose weight, you need to
-            have a balanced hormones and body processes. Many times, people
-            struggle with weight loss because their gut health has issues."
-          />
+          <meta name="google-site-verification" content="_UQAiZDb8qlmBL1_6sd_xnONNE86HEKxlGnj3gj4D-0" />
         </Head>
         <body>
           <Main />
