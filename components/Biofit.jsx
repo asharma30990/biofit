ba823898d6348d8309080de304672694112a0161
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-02-24 23:21:36 -0800

    Update Biofit.jsx

--- components/Biofit.jsx
+++ components/Biofit.jsx
@@ -93,7 +93,7 @@ export default function Biofit() {
 
             <div className="flex items-center mt-6">
               <CheckIcon className="flex-shrink-0 w-5 h-5 text-red-500" aria-hidden="true" />
-              <p className="ml-2 text-lg text-red-500 ">Attention! Get special 95% off Today + Free Shipping!</p>
+              <p className="ml-2 text-lg text-red-500 ">Attention! Get special 95% off Today + Free Shipping. Click on the Button Below and Access the Official Website at a Discount!</p>
             </div>
           </section>
         </div>
