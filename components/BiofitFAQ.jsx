a579dbd2848cb89bf76e510c01326c4b49181750
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-02-26 05:38:09 -0800

    Update BiofitFAQ.jsx

--- components/BiofitFAQ.jsx
+++ components/BiofitFAQ.jsx
@@ -152,7 +152,7 @@ export default function Example() {
           {/* Image gallery */}
           <img
             className="order-1 px-10 pt-8 lg:pt-0 lg:pr-0 lg:order-2"
-            src="/images/b4after.jpeg"
+            src="/images/biofit-result.png"
             alt=""
           />
         </div>
