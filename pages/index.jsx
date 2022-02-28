f1aeff190bda8e543cfb79bb26db4d4989ebf31b
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-02-28 23:38:30 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -97,7 +97,7 @@ export default function Index() {
                 <div className="mt-6">
                   <h2 className="text-3xl font-bold tracking-tight text-gray-900">LOSE WEIGHT FEELS GREAT.</h2>
                   <p className="mt-4 text-lg text-gray-500">
-                    After all my requirements were 100% met, We finally had the final product I called it Biofit. BioFit™ - No.#1 Weight Loss Supplement, GMP Certified, 100% - Natural Ingredients,
+                    After all my requirements were 100% met, We finally had the final product I called it biofit. BioFit™ - No. #1 Weight Loss Supplement, GMP Certified, 100% - Natural Ingredients,
                     Made in USA, FDA Approved.
                   </p>
                   <div className="w-full mt-6 sm:w-2/3">
