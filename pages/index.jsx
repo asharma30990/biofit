214c2a130cc6a6cd34869eba451f35b783ed6593
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-03-01 22:52:36 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -97,7 +97,7 @@ export default function Index() {
                 <div className="mt-6">
                   <h2 className="text-2xl font-bold tracking-tight text-gray-900">LOSE WEIGHT FEELS GREAT.</h2>
                   <p className="mt-4 text-lg font-bold tracking-normal text-gray-500">
-                    After all my requirements were 100% met, We finally had the final product I called it Biofit. BioFit™ By Chrissie Miller - No. #1 Weight Loss Supplement, GMP Certified, 100% - Fully Natural Ingredients,
+                    After all my requirements were 100% met, We finally had the final product I called it BioFit™ By Chrissie Miller. No.#1 Weight Loss Supplement, GMP Certified, 100% - Natural Ingredients,
                     Approved By FDA, Made in USA.
                   </p>
                   <div className="w-full mt-6 sm:w-2/3">
