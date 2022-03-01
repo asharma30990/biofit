cc0dcf802852c2152681532969e6d03829567c53
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-02-28 21:03:36 -0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -95,7 +95,7 @@ export default function Index() {
                   <h1 className="text-4xl font-bold tracking-normal text-purple-800 md:text-4xl">BioFit™ ®Official Website</h1>
                 </div>
                 <div className="mt-6">
-                  <h2 className="text-1xl font-bold tracking-tight text-gray-900">LOSE WEIGHT FEELS GREAT.</h2>
+                  <h2 className="text-2xl font-bold tracking-tight text-gray-900">LOSE WEIGHT FEELS GREAT.</h2>
                   <p className="mt-4 text-lg text-gray-500">
                     After all my requirements were 100% met, We finally had the final product I called it Biofit. BioFit™ - No. #1 Weight Loss Supplement, GMP Certified, 100% - Fully Natural Ingredients,
                     Approved by FDA, Made in USA.
