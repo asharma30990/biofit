51ba0a263d861250ae905987a40761866f8e2073
asharma30990 <98585283+asharma30990@users.noreply.github.com>
2022-03-02 04:00:17 -0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -96,8 +96,8 @@ export default function Index() {
                 </div>
                 <div className="mt-6">
                   <h2 className="text-2xl font-bold tracking-tight text-gray-900">LOSE WEIGHT FEELS GREAT.</h2>
-                  <p className="mt-4 text-lg font-Arial tracking-tight text-gray-500">
-                    After all my requirements were 100% met, We finally had the final product I called it BioFit™ By Chrissie Miller.               
+                  <p className="mt-4 text-lg font-Arial tracking-justify text-gray-500">
+                    After all my requirements were 100% met, We finally had the final product I called it BioFit™ By Chrissie Miller. Biofit No.#1 Weight Loss Supplement, 100% - Natural Ingrediants, GMP Certified, FDA Approved, Made in USA.                
                   </p>
                   <div className="w-full mt-6 sm:w-2/3">
                     {/* <a
